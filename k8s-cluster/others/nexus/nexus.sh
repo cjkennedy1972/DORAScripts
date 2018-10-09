04146c54860722c468e189f112c7be57c207dbdf
Raphael Londner <raphael@londner.net>
2018-10-09 13:43:22 -0700

    Update k8s-cluster/others/nexus/nexus.sh
    Added NS variable default value

--- k8s-cluster/others/nexus/nexus.sh
+++ k8s-cluster/others/nexus/nexus.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#customize the NS variable below to deploy to a different Kubernetes namespace
+NS="default"
 # Install nexus component 
 kubectl create -f nexus-claim.yaml -n ${NS} 
 kubectl create -f deployment.yaml -n ${NS} 
