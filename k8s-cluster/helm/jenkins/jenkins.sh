8f3c67ad3a43d1de5e7061bfcebc4ec5d888880c
Raphael Londner <raphael@londner.net>
2018-10-09 14:20:14 -0700

    Update k8s-cluster/helm/jenkins/jenkins.sh
    Added default value for NS variable

--- k8s-cluster/helm/jenkins/jenkins.sh
+++ k8s-cluster/helm/jenkins/jenkins.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# customize if Jenkins shouldn't be deployed in the default namespace
+NS="default" 
 # Install jenkins component 
-Kubectl create –f  jenkins-claim.yaml -n ${NS}
+kubectl create -f jenkins-claim.yaml -n ${NS}
 helm install --name ps-jenkins -f values.yaml jenkins --namespace ${NS} 
