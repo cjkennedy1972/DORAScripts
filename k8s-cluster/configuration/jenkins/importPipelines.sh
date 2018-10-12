c3e409d4539ff69a42d092a4fc96dca2c74d89d2
Raphael Londner <raphael@londner.net>
2018-10-12 16:28:09 -0700

    Added build-kernel_Stage1.xml definition file
    + jenkins-cli.jar
    + importPipelines.sh

---
+++ k8s-cluster/configuration/jenkins/importPipelines.sh
@@ -0,0 +1,8 @@
+# java -jar jenkins-cli.jar -s http://10.21.236.87:8081 -auth admin:732d3ee373dad7a3912bcfa5def98e8c get-job build-kernel_Stage1 > build-kernel_Stage1.xml
+
+#Enter your Jenkins Admin token (such as "110d236e680985f5710185c912d1cf9d91"), to be generated from User -> Configure -> API Token
+JENKINS_API_TOKEN=$1
+#Enter the URL of your Jenkins server installation, such as "http://10.21.236.87:8082"
+JENKINS_SERVER_URL=$2
+
+java -jar jenkins-cli.jar -s $JENKINS_SERVER_URL -auth admin:$JENKINS_API_TOKEN create-job build-kernel_Stage1 < build-kernel_Stage1.xml
