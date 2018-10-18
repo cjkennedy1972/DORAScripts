e0759ee6e1a05545d21c9ece3a276fb1ce5e1805
Raphael Londner <raphael@londner.net>
2018-10-17 17:40:27 -0700

    Minor install.sh fixes

--- k8s-cluster/install.sh
+++ k8s-cluster/install.sh
@@ -13,17 +13,17 @@ GITLAB_IP="10.21.236.110"
 
 #Install Sonatype Nexus
 bash ./others/nexus/nexus.sh ${NEXUS_IP} ${NS}
-bash wait-for.sh service nexus-service -n ${NS}
+bash wait-for.sh service -n ${NS} nexus-service 
 bash wait-for.sh pod -lapp=nexus -n ${NS}
 
 #Install Jenkins
 bash ./helm/jenkins/jenkins.sh ${JENKINS_IP} ${NS}
-bash wait-for.sh service pure-jenkins -n ${NS}
+bash wait-for.sh service -n ${NS} pure-jenkins 
 bash wait-for.sh pod -lapp=pure-jenkins -n ${NS}
 
 #Install GitLab
 bash ./helm/gitlab/gitlab.sh ${GITLAB_IP} ${NS}
-bash wait-for.sh service pure-gitlab -n ${NS}
+bash wait-for.sh service -n ${NS} pure-gitlab-gitlab-ce 
 bash wait-for.sh pod -lapp=pure-gitlab-redis -n ${NS}
-bash wait-for.sh pod -lapp=pure-gitlab-postgres -n ${NS}
+bash wait-for.sh pod -lapp=pure-gitlab-postgresql -n ${NS}
 bash wait-for.sh pod -lapp=pure-gitlab-gitlab-ce -n ${NS}
