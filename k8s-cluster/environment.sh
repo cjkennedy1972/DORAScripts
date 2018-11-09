90c7e75eed75c0c3428a787d6bd7f1e4d8dec756
Raphael Londner <raphael@londner.net>
2018-11-09 14:53:56 -0800

    Renamed fb-nexus to pure-nexus claim

--- k8s-cluster/environment.sh
+++ k8s-cluster/environment.sh
@@ -15,7 +15,21 @@ DOCKER_REGISTRY_PORT="5000"
 SECRET_TOKEN="{AQAAABAAAAAQockYoJutL7ZGpK6oePv79oGf7TaXymyHJ6CrQJLYBrk=}"
 
 #Change to 'fa-nexus' if you want to provision Nexus data on FlashArray
-NEXUS_CLAIM_NAME="fb-nexus"
+TARGET_STORAGE="fa"
+STORAGE_CLASS_NAME="pure-file"
+
+if [[ $TARGET_STORAGE == "fa" ]]
+then
+    STORAGE_CLASS_NAME="pure-block"
+fi
+
+IMPORT_VOLUMES_COMMENT=
+IMPORT_VOLUMES=0
+
+if [[ $IMPORT_VOLUMES == 1 ]]
+then
+    IMPORT_VOLUMES_COMMENT="#"
+fi
 
 ## Environment variables/ Params for CD jobs - Build No, vSphere creds
 ## All build  which satsify (build number % MASTER_BUILD_NO == 0 ) will launch Stage 2 + 3 VMs
--- k8s-cluster/others/nexus/nexus.sh
+++ k8s-cluster/others/nexus/nexus.sh
@@ -9,11 +9,16 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 template=`cat "$SCRIPTPATH/service.yaml" | sed "s/{{NEXUS_IP_ADDRESS}}/$NEXUS_IP/g" | sed "s/{{DOCKER_REGISTRY_PORT}}/$DOCKER_REGISTRY_PORT/g" | sed  "s/{{NEXUS_PORT}}/$NEXUS_PORT/g"`
 deploy=`cat "$SCRIPTPATH/deployment.yaml" | sed "s/{{NEXUS_CLAIM_NAME}}/$NEXUS_CLAIM_NAME/g"`
 
+claim=`cat "$SCRIPTPATH/nexus-claim.yaml" | sed "s/{{STORAGE_CLASS_NAME}}/$STORAGE_CLASS_NAME/g" | sed "s/{{IMPORT_VOLUMES_COMMENT}}/$IMPORT_VOLUMES_COMMENT/g" `
+
+echo "$claim"
+
+echo "$claim" | kubectl create -n ${NS} -f -
 #echo $SCRIPTPATH
 # Install Nexus Repository Manager resources 
-kubectl create -f "$SCRIPTPATH/nexus-claim.yaml" -n ${NS}
-echo "$template" | kubectl create -n ${NS} -f -
-echo "$deploy" | kubectl create -n ${NS} -f -
+#kubectl create -f "$SCRIPTPATH/nexus-claim.yaml" -n ${NS}
+#echo "$template" | kubectl create -n ${NS} -f -
+#echo "$deploy" | kubectl create -n ${NS} -f -
 #kubectl create -f "$SCRIPTPATH/deployment.yaml" -n ${NS}
 
 DOCKER_REGISTRY=${NEXUS_IP}:${DOCKER_REGISTRY_PORT}
