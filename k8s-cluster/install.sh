83009243c0f4eb753d56e4aab1b59c1160b18799
Raphael Londner <raphael@londner.net>
2018-11-09 02:46:46 -0800

    Simplified installation scripts

--- k8s-cluster/install.sh
+++ k8s-cluster/install.sh
@@ -5,20 +5,17 @@
 kubectl create ns ${NS}
 
 #Install Sonatype Nexus
-bash ./others/nexus/nexus.sh ${NEXUS_IP} ${NEXUS_PORT} ${NS}
+bash ./others/nexus/nexus.sh
 sleep 2
 bash wait-for.sh pod -lapp=nexus -n ${NS}
 
 #Install Jenkins
-bash ./helm/jenkins/jenkins.sh ${JENKINS_IP} ${JENKINS_PORT} ${NS}
+bash ./helm/jenkins/jenkins.sh
 sleep 2
 bash wait-for.sh pod -lapp=pure-jenkins -n ${NS}
 
-DOCKER_REGISTRY=${NEXUS_IP}:${DOCKER_REGISTRY_PORT}
-kubectl create -n ${NS} secret docker-registry jenkins-pull --docker-server=$DOCKER_REGISTRY --docker-username=admin --docker-password=admin123 --docker-email=pure@pure.pure
-
 #Install GitLab
-bash ./helm/gitlab/gitlab.sh ${GITLAB_IP} ${GITLAB_PORT} ${NS}
+bash ./helm/gitlab/gitlab.sh
 sleep 2
 bash wait-for.sh pod -lapp=pure-gitlab-redis -n ${NS}
 bash wait-for.sh pod -lapp=pure-gitlab-postgresql -n ${NS}
--- k8s-cluster/others/nexus/nexus.sh
+++ k8s-cluster/others/nexus/nexus.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
-#Set the default namespace to which we'll deploy our Kubernetes resources
-NS="pure"
-NEXUS_IP="1.1.1.1"
-NEXUS_PORT="80"
+
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-if [ ! -z $1 ]
-then
-    NEXUS_IP=$1
-else
-    echo "Aborting, the NEXUS_IP parameter is missing"
-    exit 0
-fi
-#echo $NEXUS_IP
-
-if [ ! -z $2 ]
-then
-    NEXUS_PORT=$2
-fi
-
-if [ ! -z $3 ]
-then
-    NS=$3
-fi
-#echo $NS
+. "$SCRIPTPATH"/../../environment.sh
 
 # read the yml template from a file and substitute the string 
 # {{NEXUS_IP_ADDRESS}} with the value of the NEXUS_IP variable
 template=`cat "$SCRIPTPATH/service.yaml" | sed "s/{{NEXUS_IP_ADDRESS}}/$NEXUS_IP/g" | sed "s/{{DOCKER_REGISTRY_PORT}}/$DOCKER_REGISTRY_PORT/g" | sed  "s/{{NEXUS_PORT}}/$NEXUS_PORT/g"`
+deploy=`cat "$SCRIPTPATH/deployment.yaml" | sed "s/{{NEXUS_CLAIM_NAME}}/$NEXUS_CLAIM_NAME/g"`
 
 #echo $SCRIPTPATH
 # Install Nexus Repository Manager resources 
 kubectl create -f "$SCRIPTPATH/nexus-claim.yaml" -n ${NS}
 echo "$template" | kubectl create -n ${NS} -f -
-kubectl create -f "$SCRIPTPATH/deployment.yaml" -n ${NS}
+echo "$deploy" | kubectl create -n ${NS} -f -
+#kubectl create -f "$SCRIPTPATH/deployment.yaml" -n ${NS}
+
+DOCKER_REGISTRY=${NEXUS_IP}:${DOCKER_REGISTRY_PORT}
+kubectl create -n ${NS} secret docker-registry jenkins-pull --docker-server=$DOCKER_REGISTRY --docker-username=admin --docker-password=admin123 --docker-email=pure@purestorage.com
+
