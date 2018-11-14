c30c7cbd96b414cafcf244d00e8198cda1adea63
Raphael Londner <raphael@londner.net>
2018-11-14 10:59:42 -0800

    Enhanced restore snapshots Ansible playbook

--- k8s-cluster/others/ansible/restore_snapshots.sh
+++ k8s-cluster/others/ansible/restore_snapshots.sh
@@ -7,10 +7,13 @@ JENKINS_VOL="k8s-"$JENKINS_PVC
 echo "The Nexus Pure Storage volume is " $NEXUS_VOL
 echo "The Jenkins Pure Storage volume is " $JENKINS_VOL
 
-NEXUS_SNAPSHOT=$PG_NAME.3.$NEXUS_VOL
-JENKINS_SNAPSHOT=$PG_NAME.3.$JENKINS_VOL
+SNAPSHOT_NUMBER=22
+#NEXUS_SNAPSHOT=$PG_NAME.3.$NEXUS_VOL
+#JENKINS_SNAPSHOT=$PG_NAME.3.$JENKINS_VOL
 
-echo "The Nexus snapshot name is " $NEXUS_SNAPSHOT
-echo "The Jenkins snapshot name is " $JENKINS_SNAPSHOT
+#echo "The Nexus snapshot name is " $NEXUS_SNAPSHOT
+#echo "The Jenkins snapshot name is " $JENKINS_SNAPSHOT
 
-ansible-playbook -i inventory.cfg restore_snap.yaml --extra-vars "nexus_snap=${NEXUS_SNAPSHOT} jenkins_snap=${JENKINS_SNAPSHOT}"
+#ansible-playbook -i inventory.cfg restore_snap.yaml --extra-vars "nexus_snap=${NEXUS_SNAPSHOT} jenkins_snap=${JENKINS_SNAPSHOT} snapshot_number=${SNAPSHOT_NUMBER}"
+
+ansible-playbook -i inventory.cfg restore_snap.yaml --extra-vars "nexus_snap=${NEXUS_VOL} jenkins_snap=${JENKINS_VOL} snapshot_number=${SNAPSHOT_NUMBER}"
