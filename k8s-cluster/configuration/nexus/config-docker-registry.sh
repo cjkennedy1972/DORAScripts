c00036bbbf6130b00a7de780552a8afa10a63619
Ashutosh Kumar <ashutosh@synerzip.com>
2018-11-15 10:42:06 +0530

    Ansible script for modify daemon.json (#27)
    * Added docker insecure registry
    
    * Added step to install nodejs for updateing daemon.json
    
    * Added python script to modify daemon.json file
    
    * Added step to Load var from daemon.json file
    
    * Modified scripts to use python3
    
    * Arranged files as per the config-docker-registry.sh script
    
    * Added wait to make sure all of our app containers have properly restarted

--- k8s-cluster/configuration/nexus/config-docker-registry.sh
+++ k8s-cluster/configuration/nexus/config-docker-registry.sh
@@ -5,3 +5,10 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 . "$SCRIPTPATH"/../../environment.sh
 
 ansible-playbook -i "$SCRIPTPATH"/inventory.cfg "$SCRIPTPATH/add-docker-insecure-registry.yml" --check --extra-vars "docker_insecure_registry=$NEXUS_IP:$DOCKER_REGISTRY_PORT" 
+
+# Wait to make sure all of our app containers have properly restarted
+bash ../../wait-for.sh pod -lapp=pure-gitlab-redis -n ${NS}
+bash ../../wait-for.sh pod -lapp=pure-gitlab-postgresql -n ${NS}
+bash ../../wait-for.sh pod -lapp=pure-gitlab-gitlab-ce -n ${NS}
+bash ../../wait-for.sh pod -lapp=pure-jenkins -n ${NS}
+bash ../../wait-for.sh pod -lapp=nexus -n ${NS}
