eb4f0edb7a794d59ca7532c6f0ea41c8cb202e73
Raphael Londner <raphael@londner.net>
2019-03-19 15:46:00 -0700

    GitLab Cloud Native charts customization
    Enforced v11.8.2 + moved 'certmanager-issuer' to end of gitlab-ce.yaml file

--- k8s-cluster/helm/gitlab/gitlab-ce.sh
+++ k8s-cluster/helm/gitlab/gitlab-ce.sh
@@ -1,3 +1,3 @@
-helm repo add gitlab https://charts.gitlab.io/
-helm repo update
-helm install --name gitlab -f gitlab-ce.yaml gitlab/gitlab --namespace raphael-gitlab --wait --timeout 1000
+#helm repo add gitlab https://charts.gitlab.io/
+#helm repo update
+helm install --name gitlab-test -f gitlab-ce.yaml gitlab/gitlab --namespace gitlab-test --wait --timeout 1000
