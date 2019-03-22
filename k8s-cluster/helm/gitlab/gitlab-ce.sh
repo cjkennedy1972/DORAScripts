6179855c1b85ae6a40494bf4df8be56395b93fdd
Raphael Londner <raphael@londner.net>
2019-03-21 21:28:28 -0700

    Install script updates
    Minor updates and fixes

--- k8s-cluster/helm/gitlab/gitlab-ce.sh
+++ k8s-cluster/helm/gitlab/gitlab-ce.sh
@@ -2,7 +2,7 @@
 #helm repo update
 NS=gitlabtest
 kubectl create ns ${NS}
-kubectl create secret generic ${NS}-gitlab-initial-root-password --from-literal=password=admin123 -n ${NS}
+kubectl create secret generic easy-gitlab-initial-root-password --from-literal=password=admin123 -n ${NS}
 #helm install --name ${NS} -f gitlab-ce.yaml gitlab/gitlab --namespace ${NS} --wait --timeout 1000
 helm install --name ${NS} -f gitlab-ce.yaml gitlab/gitlab --namespace ${NS} --wait --timeout 1000
 --set postgresql.persistence.storageClass=kontena-storage-block \
--- k8s-cluster/helm/prometheus/prometheus.sh
+++ k8s-cluster/helm/prometheus/prometheus.sh
@@ -4,9 +4,6 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 . "$SCRIPTPATH"/../../environment.sh
 
-
-
-# Install Prometheus Compoent using Helm chart
-# No PVC is used
+# Install Prometheus using Helm chart
 template=`cat "$SCRIPTPATH/value.yaml"`
 echo "$template" | helm install --name pure-prometheus-${NS} stable/prometheus --namespace ${NS} -f -
