2e31ea70687d2931c4195e42a47291ab92772149
Raphael Londner <raphael@londner.net>
2019-03-26 18:29:41 -0700

    Update gitlab-ce.sh
    Typo fix

--- k8s-cluster/helm/gitlab/gitlab-ce.sh
+++ k8s-cluster/helm/gitlab/gitlab-ce.sh
@@ -8,8 +8,8 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 kubectl create secret generic easy-gitlab-initial-root-password --from-literal=password=admin123 -n ${NS}
 #helm install --name ${NS} -f gitlab-ce.yaml gitlab/gitlab --namespace ${NS} --wait --timeout 1000
 helm install --name pure-gitlab-${NS} gitlab/gitlab -f "${SCRIPTPATH}"/gitlab-ce.yaml --namespace ${NS} --wait --timeout 1000 \
---set hosts.domain=${GITLAB_DOMAIN} \
---set hosts.hostSuffix=${GITLAB_SUFFIX} \
+--set global.hosts.domain=${GITLAB_DOMAIN} \
+--set global.hosts.hostSuffix=${GITLAB_SUFFIX} \
 --set postgresql.persistence.storageClass=${STORAGE_CLASS_NAME} \
 --set redis.persistence.storageClass=${STORAGE_CLASS_NAME} \
 --set minio.persistence.storageClass=${STORAGE_CLASS_NAME} \
