eac6745b0579bd3ae5f0c84afa04121dd552c568
Raphael Londner <raphael@londner.net>
2019-04-04 16:47:07 -0700

    Update environment.sh
    Switched to http (not https) for GitLab

--- k8s-cluster/environment.sh
+++ k8s-cluster/environment.sh
@@ -72,14 +72,14 @@ METAL_LB_IP_CIDR="10.21.236.96-10.21.236.107"
 GITLAB_IP="10.21.236.89"
 GITLAB_PORT="8181"
 GITLAB_DOMAIN="puretec.purestorage.com"
-GITLAB_HTTP_PREFIX="https" #replace with "http" if using HTTP
+GITLAB_HTTP_PREFIX="http" #replace with "http" if using HTTP
 GITLAB_SUFFIX="dev" #if non-empty, creates a GitLab instance accessible at GITLAB_HTTP_PREFIX://gitlab-GITLAB_SUFFIX.GITLAB_DOMAIN (for instance, http://gitlab-staging.puretec.purestorage.com). Otherwise, GitLab is available at GITLAB_HTTP_PREFIX://gitlab.GITLAB_DOMAIN
 #GITLAB_FQDN="git.puretec.purestorage.com"
 
 #Create and paste below your GitLab API Access Token (with 'api' scope) generated from http://<GITLAB_IP>:<GITLAB_PORT>/profile/personal_access_tokens
-GITLAB_TOKEN="EuU-YE2sEFZoavoy1d2s"
+GITLAB_TOKEN="oVvsJqeV-ybCe2uMU7bP"
 #Create and paste below your Jenkins API  Token generated from http://<JENKINS_IP>:<JENKINS_PORT>/user/admin/configure
-JENKINS_TOKEN="11419ca55eb242a22a548140e5099a05e7"
+JENKINS_TOKEN="11723a39ff9d413775c52f2ba2e3dfff66"
 
 ## Grafana settings
 GRAFANA_HOSTNAME="grafana.puretec.purestorage.com"
