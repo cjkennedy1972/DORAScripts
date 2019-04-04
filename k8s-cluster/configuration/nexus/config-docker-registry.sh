6a2050ee30425736095a78ac527c824a0c2a1d9a
Raphael Londner <raphael@londner.net>
2019-04-04 16:45:42 -0700

    Update config-docker-registry.sh
    Removed --check parameter to make sure Docker insecure registries get configured

--- k8s-cluster/configuration/nexus/config-docker-registry.sh
+++ k8s-cluster/configuration/nexus/config-docker-registry.sh
@@ -4,7 +4,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 . "$SCRIPTPATH"/../../environment.sh
 
-ansible-playbook -i "$SCRIPTPATH"/inventory.cfg "$SCRIPTPATH/add-docker-insecure-registry.yml" --check --extra-vars "docker_insecure_registry=$NEXUS_IP:$DOCKER_REGISTRY_PORT" 
+ansible-playbook -i "$SCRIPTPATH"/inventory.cfg "$SCRIPTPATH/add-docker-insecure-registry.yml" --extra-vars "docker_insecure_registry=$NEXUS_IP:$DOCKER_REGISTRY_PORT" 
 
 # Wait to make sure all of our app containers have properly restarted
 bash "$SCRIPTPATH"/../../wait-for.sh pod -lapp=pure-gitlab-${NS}-redis -n ${NS}
