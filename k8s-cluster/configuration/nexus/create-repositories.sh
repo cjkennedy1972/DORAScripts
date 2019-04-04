7db37969cf3dd66f55f3ff65a49938cd3bf62125
Raphael Londner <raphael@londner.net>
2019-04-03 19:05:03 -0700

    Update create-repositories.sh
    - Used FQDN instead of IP and PORT

--- k8s-cluster/configuration/nexus/create-repositories.sh
+++ k8s-cluster/configuration/nexus/create-repositories.sh
@@ -9,12 +9,12 @@ ADMIN_USERNAME="admin"
 ADMIN_PWD="admin123"
 
 curl -X POST -u $ADMIN_USERNAME:$ADMIN_PWD --header 'Content-Type: application/json' \
-    http://$NEXUS_IP:$NEXUS_PORT/service/rest/v1/script \
+    http://$NEXUS_FQDN/service/rest/v1/script \
     -d '{"name":"raw-pure'$RANDOM_NUMBER'","type":"groovy","content":"repository.createRawHosted('\''builds-store'\'');"}'
 curl -X POST -u $ADMIN_USERNAME:$ADMIN_PWD --header "Content-Type: text/plain" 'http://'$NEXUS_IP':'$NEXUS_PORT'/service/rest/v1/script/raw-pure'$RANDOM_NUMBER'/run'
 
 
 curl -X POST -u $ADMIN_USERNAME:$ADMIN_PWD --header 'Content-Type: application/json' \
-    http://$NEXUS_IP:$NEXUS_PORT/service/rest/v1/script \
+    http://$NEXUS_FQDN/service/rest/v1/script \
     -d '{"name":"docker-pure'$RANDOM_NUMBER'","type":"groovy","content":"repository.createDockerHosted('\''docker'\'', '$DOCKER_REGISTRY_PORT', 5001, '\''default'\'', true, true);"}'
 curl -X POST -u $ADMIN_USERNAME:$ADMIN_PWD --header "Content-Type: text/plain" 'http://'$NEXUS_IP':'$NEXUS_PORT'/service/rest/v1/script/docker-pure'$RANDOM_NUMBER'/run'
