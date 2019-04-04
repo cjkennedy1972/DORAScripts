5b473180892da34546ec156b528522c1cbc2e9ba
Raphael Londner <raphael@londner.net>
2019-04-03 19:01:14 -0700

    Update environment.sh
    Fixed STORAGE_CLASS evaluation

--- k8s-cluster/environment.sh
+++ k8s-cluster/environment.sh
@@ -15,14 +15,14 @@ TARGET_STORAGE="fb" #"fa" for on-prem FlashArray deployment, "fb" for on-prem Fl
 TARGET_DEPLOYMENT="onprem"
 PV_ACCESS_MODE="ReadWriteMany"
 
-if [[ $TARGET_STORAGE == "fa" ]]
+if [ $TARGET_STORAGE == "fa" ]
 then
     STORAGE_CLASS_NAME="pure-block"
     PV_ACCESS_MODE="ReadWriteOnce"
 elif [ $TARGET_STORAGE == "fb" ]
 then
     STORAGE_CLASS_NAME="pure-file"
-     PV_ACCESS_MODE="ReadWriteMany"
+    PV_ACCESS_MODE="ReadWriteMany"
 else
     STORAGE_CLASS_NAME="kontena-storage-block"
     PV_ACCESS_MODE="ReadWriteOnce"
@@ -73,19 +73,22 @@ GITLAB_IP="10.21.236.89"
 GITLAB_PORT="8181"
 GITLAB_DOMAIN="puretec.purestorage.com"
 GITLAB_HTTP_PREFIX="https" #replace with "http" if using HTTP
-GITLAB_SUFFIX="" #if non-empty, creates a GitLab instance accessible at GITLAB_HTTP_PREFIX://gitlab-GITLAB_SUFFIX.GITLAB_DOMAIN (for instance, http://gitlab-staging.puretec.purestorage.com). Otherwise, GitLab is available at GITLAB_HTTP_PREFIX://gitlab.GITLAB_DOMAIN
+GITLAB_SUFFIX="dev" #if non-empty, creates a GitLab instance accessible at GITLAB_HTTP_PREFIX://gitlab-GITLAB_SUFFIX.GITLAB_DOMAIN (for instance, http://gitlab-staging.puretec.purestorage.com). Otherwise, GitLab is available at GITLAB_HTTP_PREFIX://gitlab.GITLAB_DOMAIN
 #GITLAB_FQDN="git.puretec.purestorage.com"
 
 #Create and paste below your GitLab API Access Token (with 'api' scope) generated from http://<GITLAB_IP>:<GITLAB_PORT>/profile/personal_access_tokens
-GITLAB_TOKEN="jLuFaJw3NzkkfxacM5md"
+GITLAB_TOKEN="EuU-YE2sEFZoavoy1d2s"
 #Create and paste below your Jenkins API  Token generated from http://<JENKINS_IP>:<JENKINS_PORT>/user/admin/configure
-JENKINS_TOKEN="11546ed26f156524be9f06618ab52dba2d"
+JENKINS_TOKEN="11419ca55eb242a22a548140e5099a05e7"
+
+## Grafana settings
+GRAFANA_HOSTNAME="grafana.puretec.purestorage.com"
 
 ## Enable this if you want to install or uninstall Prometheus/Grafana
-ENABLE_MONITORING="false"
+ENABLE_MONITORING="true"
 
 ## Enable this if you want to install MetalLB + NGINX Ingress
-METAL_LB_NGINX_INGRESS="false"
+METAL_LB_NGINX_INGRESS="true"
 
 ## If set to true, will delete MetalLB (from metallb-system namespace) and NGINX Ingress (from ingress-nginx namespace)
 UNINSTALL_PREREQUISITES="false"
