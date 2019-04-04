78a9b94f3ce430849f5f3b8044699377e3fe8429
Raphael Londner <raphael@londner.net>
2019-04-04 16:46:39 -0700

    Update configure.sh
    Added namespace required to craft the proper service name for GitLab Unicorn (in importPipelines.sh)

--- k8s-cluster/configure.sh
+++ k8s-cluster/configure.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-bash ./configuration/nexus/config-docker-registry.sh
+. environment.sh
+
+#bash ./configuration/nexus/config-docker-registry.sh
 
 bash ./configuration/nexus/create-repositories.sh
 
 bash ./configuration/gitlab/createProjects.sh
 
-bash ./configuration/jenkins/importPipelines.sh 
+bash ./configuration/jenkins/importPipelines.sh ${NS}
 
 bash ./configuration/nexus/create-and-push-docker-images.sh
