0aa448c11d5952ba0e15a8474c3f9de4de296334
Raphael Londner <raphael@londner.net>
2019-04-03 19:06:57 -0700

    Update prometheus.sh
    Fixed values.yaml reference

--- k8s-cluster/helm/prometheus/prometheus.sh
+++ k8s-cluster/helm/prometheus/prometheus.sh
@@ -5,5 +5,6 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 . "$SCRIPTPATH"/../../environment.sh
 
 # Install Prometheus using Helm chart
-template=`cat "$SCRIPTPATH/value.yaml"`
-echo "$template" | helm install --name pure-prometheus-${NS} stable/prometheus --namespace ${NS} -f -
+template=`cat "$SCRIPTPATH/values.yaml"`
+helm install --name pure-prometheus-${NS} stable/prometheus --namespace ${NS} -f "$SCRIPTPATH"/values.yaml
+#echo "$template" | helm install --name pure-prometheus-${NS} stable/prometheus --namespace ${NS} -f -
