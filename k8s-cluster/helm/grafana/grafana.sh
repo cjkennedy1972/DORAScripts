c4ec0f261d33bff0abd3ec92cd91aa03262308e3
Ashutosh Kumar <ashutosh@synerzip.com>
2019-04-05 18:35:35 +0530

    Modified Grafana charts and scripts Added Grafana service

--- k8s-cluster/helm/grafana/grafana.sh
+++ k8s-cluster/helm/grafana/grafana.sh
@@ -8,9 +8,10 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 # Install Grafana Compoent using Helm chart
 # No PVC is used
-kubectl create -n ${NS} secret generic grafana-creds  --from-literal=admin-user=admin  --from-literal=admin-password=admin
+kubectl create -n ${MONITORING_NS} secret generic grafana-creds  --from-literal=admin.userKey=admin  --from-literal=admin.passwordKey=admin
 
-#template=`cat "$SCRIPTPATH/values.yaml"`
-helm install --name pure-grafana-${NS} stable/grafana --namespace ${NS} -f "${SCRIPTPATH}"/values.yaml \
---set ingress.hosts[0]=${GRAFANA_HOSTNAME}
-#echo "$template" | helm install --name pure-grafana-${NS} stable/grafana --namespace ${NS} -f -
+template=`cat "$SCRIPTPATH/values.yaml"`
+echo "$template" | helm install --name pure-grafana-${NS} stable/grafana --namespace ${MONITORING_NS} -f -
+
+# Create Grafana Service
+kubectl create -n ${MONITORING_NS} -f "$SCRIPTPATH/service.yaml"
