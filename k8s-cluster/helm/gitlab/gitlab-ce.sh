503a837b876c29fad7f7fc063b49a853a495fefe
Ashutosh Kumar <ashutosh@synerzip.com>
2019-04-05 18:35:09 +0530

    Modified Gitlab chart configuration and creation script to pick storage class at execution time.

--- k8s-cluster/helm/gitlab/gitlab-ce.sh
+++ k8s-cluster/helm/gitlab/gitlab-ce.sh
@@ -4,14 +4,11 @@ helm repo update
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 . "$SCRIPTPATH"/../../environment.sh
 
-#kubectl create ns ${NS}
+template=`cat "$SCRIPTPATH/gitlab-ce.yaml" | sed "s/{{NS}}/$NS/g" | sed "s/{{STORAGE_CLASS_NAME}}/$STORAGE_CLASS_NAME/g" | sed "s/{{PV_ACCESS_MODE}}/$PV_ACCESS_MODE/g"`
+
 kubectl create secret generic easy-gitlab-initial-root-password --from-literal=password=admin123 -n ${NS}
 #helm install --name ${NS} -f gitlab-ce.yaml gitlab/gitlab --namespace ${NS} --wait --timeout 1000
-helm install --name pure-gitlab-${NS} gitlab/gitlab -f "${SCRIPTPATH}"/gitlab-ce.yaml --version 1.7.2 --namespace ${NS} --wait --timeout 1000 \
+echo "$template" | helm install --name pure-gitlab-${NS} gitlab/gitlab --version 1.7.2 --namespace ${NS} \
 --set global.hosts.domain=${GITLAB_DOMAIN} \
 --set global.hosts.hostSuffix=${GITLAB_SUFFIX} \
---set postgresql.persistence.storageClass=${STORAGE_CLASS_NAME} \
---set redis.persistence.storageClass=${STORAGE_CLASS_NAME} \
---set minio.persistence.storageClass=${STORAGE_CLASS_NAME} \
---set gitlab.gitaly.persistence.storageClass=${STORAGE_CLASS_NAME}
-#--set hosts.externalIP=${GITLAB_IP} \
+-f -
